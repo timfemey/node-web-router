5c68b1e8518bec48beef62ca147fb95be493e3f8
Obafemi <81351455+timfemey@users.noreply.github.com>
2022-05-07 13:03:54 +0100

    Update better path creation

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const node_env = process.env.NODE_ENV;
+const authStatic = path.resolve(__dirname + "/prod/auth");
+const auth = path.resolve(__dirname + "/prod/auth/index.html");
 
 app.set("port", PORT);
 app.set("env", node_env);
 
 app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname + "/build/")));
-  res.status(200).sendFile(path.resolve(__dirname + "/build/index.html"));
+  app.use(express.static(authStatic));
+  res.status(200).sendFile(auth);
 });
 app.listen(PORT, () => console.log(`Running on ${PORT}!`));
